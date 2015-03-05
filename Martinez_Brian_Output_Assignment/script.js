1d86d61ed22a753f112c34cebab8d9574ba5b4e6
b-martinez <bamartinez@fullsail.edu>
2015-03-05 01:21:38 -0800

    Final comments
    Added final comments to each line of code.

--- Martinez_Brian_Output_Assignment/script.js
+++ Martinez_Brian_Output_Assignment/script.js
@@ -1,16 +1,16 @@
 /**Brian Martinez
 SDI 03/05/2015
 201503-01
-Output Assignment**/
+Output Assignment**/ //this is my header comments
 
-var myStringExample = "I play rugby and am a flanker.";
-var mySon = 9;
-var myAnswer = true;
+var myStringExample = "I play rugby and am a flanker."; //this is my example for a string variable
+var mySon = 9; //this is my example for a number variable
+var myAnswer = true; //this is my example for a Boolean variable
 
-console.log("This is my submission for the Output Assignment.");
-console.log("My name is Brian Martinez and " + myStringExample);
-console.log("I have a son that is " + mySon + " months old.");
-console.log("A question for myself is did I code this assignment correctly? Yes this is, " + myAnswer + ".");
+console.log("This is my submission for the Output Assignment."); //sent string to console
+console.log("My name is Brian Martinez and " + myStringExample); //concatenated my string variable and sent to output
+console.log("I have a son that is " + mySon + " months old."); //concatenated string and number variable and sent to output
+console.log("A question for myself is did I code this assignment correctly? Yes this is, " + myAnswer + "."); //concatenated a string and Boolean variable, sent to output
 
 
 
