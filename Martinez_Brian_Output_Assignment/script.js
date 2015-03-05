09dbc840f836fffeca2dd6cf6b3af2d5a1896d81
b-martinez <bamartinez@fullsail.edu>
2015-03-05 01:16:32 -0800

    Output
    Sent output to console. (Used Firebug)

--- Martinez_Brian_Output_Assignment/script.js
+++ Martinez_Brian_Output_Assignment/script.js
@@ -5,7 +5,13 @@ Output Assignment**/
 
 var myStringExample = "I play rugby and am a flanker.";
 var mySon = 9;
-var myBoolean = true;
+var myAnswer = true;
+
+console.log("This is my submission for the Output Assignment.");
+console.log("My name is Brian Martinez and " + myStringExample);
+console.log("I have a son that is " + mySon + " months old.");
+console.log("A question for myself is did I code this assignment correctly? Yes this is, " + myAnswer + ".");
+
 
 
 
