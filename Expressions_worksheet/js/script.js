335e3cb1050342162869bb879a5fa8a860a3554d
b-martinez <bamartinez@fullsail.edu>
2015-03-10 23:38:30 -0700

    Expressions Worksheet
    Dog Years Example

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,21 @@
+/*
+Brian Martinez
+March 9, 2015
+Expressions Worksheet
+ */
+
+//Dog Years Example
+//Figure out Sparky's age from human years to dog years
+
+//Variables
+var sparkyHuman;
+
+//Prompts
+sparkyHuman = prompt("Enter Sparky's Age here:");
+
+var sparkyDog = Number(sparkyHuman) * 7;
+
+//Output
+console.log("Sparky is " + sparkyHuman + " " + "human years old which is " + sparkyDog + " " + "in dog years.");
+
+
