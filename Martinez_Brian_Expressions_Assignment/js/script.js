66c211b8f79443711011942f55cccda5e9382432
b-martinez <bamartinez@fullsail.edu>
2015-03-12 17:34:21 -0700

    Added Output
    Added outputs to console.log

--- Martinez_Brian_Expressions_Assignment/js/script.js
+++ Martinez_Brian_Expressions_Assignment/js/script.js
@@ -18,7 +18,7 @@ var inputArray = [currentWeight, goalWeight, daysToGoal];
 
 poundsPerDay = (inputArray[0] - inputArray[1]) / inputArray[2];
 
-console.log(poundsPerDay);
+console.log("In order to reach your goal weight in " + daysToGoal + " days. You would need to lose " + poundsPerDay + " lbs. per day.");
 
 var incWeek = daysToGoal += 7;
 
@@ -26,6 +26,22 @@ var decWeek = daysToGoal -= 14;
 
 //console.log(incWeek, decWeek);
 
+var extraDays = (inputArray[0] - inputArray[1]) / incWeek;
 
+var decDays = (inputArray[0] - inputArray[1]) / decWeek;
+
+
+//Output
+
+console.log("If you INCREASED your days by a week (" + incWeek + " days). You could lose " + extraDays + "lbs. per day.");
+
+console.log("If you DECREASED your days by a week (" + decWeek + " days). You would need to lose " + decDays + "lbs. per day.");
+
+
+//Test Results
+/* I typed in 220 for CURRENT weight and 205 for GOAL weight and 30 days to goal and my calculator told me to lose 0.5 pounds per day to reach my goal.
+*I typed in 300 for CURRENT weight and 210 for GOAL weight and 30 days to goal and my calculator told me to lose 3 pounds per day to reach my goal.
+* I typed in 169 for CURRENT weight and 150 for GOAL weight and 33 days to goal and my calculator told me to lose 0.575 pounds per day to reach my goal.
+* */
 
 
