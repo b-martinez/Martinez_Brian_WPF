c35a678ce3c2504eba168c1f46645e82911f6729
b-martinez <bamartinez@fullsail.edu>
2015-03-12 17:38:05 -0700

    Comments
    Added comments on each line.

--- Martinez_Brian_Expressions_Assignment/js/script.js
+++ Martinez_Brian_Expressions_Assignment/js/script.js
@@ -7,35 +7,35 @@ Expressions Assignment
 //alert("Test"); used to test link to html file
 
 //Variables
-var currentWeight = Number(prompt("Enter your CURRENT weight here:"));
-var goalWeight = Number(prompt("Enter your GOAL weight here:"));
-var daysToGoal = Number(prompt("How many days do you have to reach your goal?"));
-var poundsPerDay;
+var currentWeight = Number(prompt("Enter your CURRENT weight here:")); //creating variable using prompt
+var goalWeight = Number(prompt("Enter your GOAL weight here:")); //creating variable using prompt
+var daysToGoal = Number(prompt("How many days do you have to reach your goal?")); //creating variable using prompt
+var poundsPerDay; //my result variable
 
 //Operations
 
-var inputArray = [currentWeight, goalWeight, daysToGoal];
+var inputArray = [currentWeight, goalWeight, daysToGoal]; //created an array to use for calculations
 
-poundsPerDay = (inputArray[0] - inputArray[1]) / inputArray[2];
+poundsPerDay = (inputArray[0] - inputArray[1]) / inputArray[2]; //formula to find result variable using my array.
 
-console.log("In order to reach your goal weight in " + daysToGoal + " days. You would need to lose " + poundsPerDay + " lbs. per day.");
+console.log("In order to reach your goal weight in " + daysToGoal + " days. You would need to lose " + poundsPerDay + " lbs. per day."); //printing out to console my results.
 
-var incWeek = daysToGoal += 7;
+var incWeek = daysToGoal += 7; //using assignment operator to increase
 
-var decWeek = daysToGoal -= 14;
+var decWeek = daysToGoal -= 14; //using assignment operator to decrease
 
 //console.log(incWeek, decWeek);
 
-var extraDays = (inputArray[0] - inputArray[1]) / incWeek;
+var extraDays = (inputArray[0] - inputArray[1]) / incWeek; //formula to find result using new days
 
-var decDays = (inputArray[0] - inputArray[1]) / decWeek;
+var decDays = (inputArray[0] - inputArray[1]) / decWeek; //formula to find result using new days
 
 
 //Output
 
-console.log("If you INCREASED your days by a week (" + incWeek + " days). You could lose " + extraDays + "lbs. per day.");
+console.log("If you INCREASED your days by a week (" + incWeek + " days). You could lose " + extraDays + "lbs. per day."); //printing out my new results as suggestions
 
-console.log("If you DECREASED your days by a week (" + decWeek + " days). You would need to lose " + decDays + "lbs. per day.");
+console.log("If you DECREASED your days by a week (" + decWeek + " days). You would need to lose " + decDays + "lbs. per day."); //printing out my new results as suggestions
 
 
 //Test Results
