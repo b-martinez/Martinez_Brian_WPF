deb2d00dba40b4d1334235eb7a1cfcc845b0a6aa
b-martinez <bamartinez@fullsail.edu>
2015-03-12 17:02:15 -0700

    Added Calculations
    Added formulas for calculator and tested with console.log output

--- Martinez_Brian_Expressions_Assignment/js/script.js
+++ Martinez_Brian_Expressions_Assignment/js/script.js
@@ -12,6 +12,13 @@ var goalWeight = Number(prompt("Enter your GOAL weight here:"));
 var daysToGoal = Number(prompt("How many days do you have to reach your goal?"));
 var poundsPerDay;
 
+//Operations
+
+var inputArray = [currentWeight, goalWeight, daysToGoal];
+
+poundsPerDay = (inputArray[0] - inputArray[1]) / inputArray[2];
+
+console.log(poundsPerDay);
 
 
 
