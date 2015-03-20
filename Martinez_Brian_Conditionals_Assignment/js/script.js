b04568051f668babdf2f79d0adbfcd3b19e59ea4
b-martinez <bamartinez@fullsail.edu>
2015-03-19 18:20:37 -0700

    Adjusted calculations and prompts

--- Martinez_Brian_Conditionals_Assignment/js/script.js
+++ Martinez_Brian_Conditionals_Assignment/js/script.js
@@ -9,12 +9,29 @@
 
 //Variables
 var childsAge = Number(prompt("Enter your CURRENT age here:")); //creating variable using prompt
-var parentAge = Number(prompt("Enter your PARENT'S age here:")); //creating variable using prompt
-var wParent = prompt("Is your parent with you currently?"); //creating variable using prompt
-var entry; //my result variable
-
-console.log(childsAge, parentAge);
+var entry = true; //my result variable
 
 //Conditionals
 
-(childsAge >= 18) ? console.log ("You are of age, you MAY enter the venue! Have fun!") : console.log("I'm sorry kid you need to grow up some more!");
+var timeRemaining = 18 - childsAge;
+
+(childsAge >= 18) ? console.log ("You are of age, you MAY enter the venue! Have fun!") : console.log("I'm sorry kid you need to wait another " + timeRemaining + " years to come in alone.");
+
+if(childsAge < 18){
+    var wParent = prompt("Is your parent currently with you? Yes if True and No if False.");
+    if(wParent === ""){
+        wParent = prompt("Please enter if your parent(s) are currently here with you? Yes if True and No if False.");
+        if(childsAge < 18 && wParent === "Yes"){
+            console.log("Great your parents are here! You may enter!");
+        }else if(childsAge < 18 && wParent === "No"){
+            console.log("I'm sorry you are underage and your parent's are not here. You may not enter the venue.");
+        }else{
+
+        }
+
+    }
+}else{
+    console.log("You are of age, you may enter the venue.");
+}
+    console.log("Thank you, please come again soon!");
+
