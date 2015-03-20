d366cd8313e60877f039a49e1bb7e6d87abdb53b
b-martinez <bamartinez@fullsail.edu>
2015-03-19 17:49:36 -0700

    Added Variables
    created variables for calculator

--- Martinez_Brian_Conditionals_Assignment/js/script.js
+++ Martinez_Brian_Conditionals_Assignment/js/script.js
@@ -6,3 +6,12 @@
  */
 
 //alert("Test"); //used to test link to html file
+
+//Variables
+var childsAge = Number(prompt("Enter your CURRENT age here:")); //creating variable using prompt
+var parentAge = Number(prompt("Enter your PARENT'S age here:")); //creating variable using prompt
+var wParent = prompt("Is your parent with you currently?"); //creating variable using prompt
+var entry; //my result variable
+
+console.log(childsAge, parentAge);
+
