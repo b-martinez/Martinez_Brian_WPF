7ead70552b4aa1dfebed780704c7ccdd6ae47791
b-martinez <bamartinez@fullsail.edu>
2015-03-19 18:57:54 -0700

    added token calculation

--- Martinez_Brian_Conditionals_Assignment/js/script.js
+++ Martinez_Brian_Conditionals_Assignment/js/script.js
@@ -8,30 +8,65 @@
 //alert("Test"); //used to test link to html file
 
 //Variables
-var childsAge = Number(prompt("Enter your CURRENT age here:")); //creating variable using prompt
-var entry = true; //my result variable
+var entry = prompt("Did you want to enter the venue today?");
+if(entry === "") {
+    entry = prompt("I'm sorry I didn't catch that. Did you want to enter the venue today?");
+    if (entry === "Yes"){
+        var childsAge = Number(prompt("Enter your CURRENT age here:"));
+        var timeRemaining = 18 - childsAge;
 
-//Conditionals
+        if(childsAge < 18){
+            var wParent = prompt("Is your parent currently with you? Yes if True and No if False.");
+            if(wParent === ""){
+                wParent = prompt("Please enter if your parent(s) are currently here with you? Yes if True and No if False.");
+                if(childsAge < 18 && wParent === "Yes"){
+                    console.log("Great your parents are here! You may enter! Remember you have another " + timeRemaining + " years before you can come alone.");
+                    var tokens = prompt("Did you want tokens to play the games inside? Yes or No.");
+                    if (tokens === ""){
+                        tokens = prompt("Did you want to purchase tokens today? Yes or No.");
+                        if (tokens === "Yes"){
+                            var amountTokens = Number(prompt("How many tokens did you want to purchase? (Note: Each token costs $0.25) Enter a number."));
+                            var tokensTotal = amountTokens * 0.25;
+                            console.log("You will purchase " + amountTokens + " tokens. At a total cost of $" + tokensTotal + ".");
 
-var timeRemaining = 18 - childsAge;
+                        }else{
+                            console.log("Maybe next time!");
+                        }
+                    }
+                }else if(childsAge < 18 && wParent === "No"){
+                    console.log("I'm sorry you are underage and your parent's are not here. You may not enter the venue. You have to wait another " + timeRemaining + " years before you can come alone.");
+                }else{
 
-(childsAge >= 18) ? console.log ("You are of age, you MAY enter the venue! Have fun!") : console.log("I'm sorry kid you need to wait another " + timeRemaining + " years to come in alone.");
+                }
 
-if(childsAge < 18){
-    var wParent = prompt("Is your parent currently with you? Yes if True and No if False.");
-    if(wParent === ""){
-        wParent = prompt("Please enter if your parent(s) are currently here with you? Yes if True and No if False.");
-        if(childsAge < 18 && wParent === "Yes"){
-            console.log("Great your parents are here! You may enter!");
-        }else if(childsAge < 18 && wParent === "No"){
-            console.log("I'm sorry you are underage and your parent's are not here. You may not enter the venue.");
+            }
         }else{
+            console.log("You are of age, you may enter the venue.");
+            var tokens = prompt("Did you want tokens to play the games inside? Yes or No.");
+            if (tokens === "") {
+                tokens = prompt("Did you want to purchase tokens today? Yes or No.");
+                if (tokens === "Yes"){
+                    var amountTokens = Number(prompt("How many tokens did you want to purchase? (Note: Each token costs $0.25) Enter a number."));
+                    var tokensTotal = amountTokens * 0.25;
+                    console.log("You will purchase " + amountTokens + " tokens. At a total cost of $" + tokensTotal + ".");
 
+                }else{
+                    console.log("Maybe next time!");
+                }
+            }
         }
-
+    }else{
+        console.log("Thank you come again!");
     }
-}else{
-    console.log("You are of age, you may enter the venue.");
 }
-    console.log("Thank you, please come again soon!");
+
+
+
+
+
+
+
+
+
+
 
