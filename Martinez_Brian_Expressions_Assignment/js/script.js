5e2dfa2feb0759b7a0f2f70e7546b6648ce5dfc8
b-martinez <bamartinez@fullsail.edu>
2015-04-15 23:26:29 -0700

    Variables Added
    Variables added to assignment

---
+++ Martinez_Brian_Expressions_Assignment/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Brian Martinez
+ 04/16/2015
+ Expressions Assignment
+ Weight Loss Calculator (Per Day)
+ */
+
+//alert("Test");
+
+//Variables
+var currentWeight = Number(prompt("Enter your CURRENT weight below:"));
+var goalWeight = Number(prompt("Enter your GOAL weight below:"));
+var daysToGoal = Number(prompt("How many days do you have to reach your goal?"));
+var poundsPerDay;
+
