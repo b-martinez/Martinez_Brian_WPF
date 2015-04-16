457e544597a9b0e2b628a2db10036be9b0146385
b-martinez <bamartinez@fullsail.edu>
2015-04-16 00:11:00 -0700

    Sparky Dog Example

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,23 @@
+/*
+ * Brian Martinez
+ * 04/16/2015
+ * Expressions worksheet
+ */
+
+
+//alert("Test!");
+
+//Dog Years Example
+//Figure out Sparky's age from human years to dog years
+
+//Variables
+var sparkyHuman;
+
+//Prompts
+sparkyHuman = prompt("Enter Sparky's Age here:");
+
+var sparkyDog = Number(sparkyHuman) * 7;
+
+//Output
+console.log("Sparky is " + sparkyHuman + " " + "human years old which is " + sparkyDog + " " + "in dog years.");
+
