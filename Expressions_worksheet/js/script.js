ffa15406b42a72749e61c9fdea060cbd51fed5ef
b-martinez <bamartinez@fullsail.edu>
2015-04-16 00:14:34 -0700

    Pizza Slice Example 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -21,3 +21,18 @@ var sparkyDog = Number(sparkyHuman) * 7;
 //Output
 console.log("Sparky is " + sparkyHuman + " " + "human years old which is " + sparkyDog + " " + "in dog years.");
 
+//Slice of Pizza Example 1
+
+//Variables for number of slices per pizza, people at the party, and number of pizzas ordered
+var slices = Number(prompt("Enter number of slices per pizza:"));
+var attendees = Number(prompt("How many people attended the pizza party?"));
+var pizzas = Number(prompt("How many pizzas were ordered:"));
+
+//Operations
+var slicesPerPerson = (slices * pizzas) / attendees;
+
+//Output
+console.log("Each person ate " + slicesPerPerson + " " + "slices of pizza at the party.");
+
+
+
