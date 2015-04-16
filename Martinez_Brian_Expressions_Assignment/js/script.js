78ae454d754673e03ee994c1cb2c3244384ba786
b-martinez <bamartinez@fullsail.edu>
2015-04-15 23:33:49 -0700

    Initial Result Expression
    Found initial result variable

--- Martinez_Brian_Expressions_Assignment/js/script.js
+++ Martinez_Brian_Expressions_Assignment/js/script.js
@@ -13,3 +13,10 @@ var goalWeight = Number(prompt("Enter your GOAL weight below:"));
 var daysToGoal = Number(prompt("How many days do you have to reach your goal?"));
 var poundsPerDay;
 
+//Operations
+var inputArray = [currentWeight, goalWeight, daysToGoal];
+
+poundsPerDay = (inputArray[0] - inputArray[1]) / inputArray[2];
+
+console.log("In order to reach your goal weight in " + daysToGoal + " days. You would need to lose " + poundsPerDay + " lbs. per day.");
+
