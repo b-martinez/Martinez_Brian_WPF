95e999cdb7f5642d21a50889825aab710ac9f5cd
b-martinez <bamartinez@fullsail.edu>
2015-04-23 18:58:04 -0700

    created initial variables and prompts
    First 2 prompts created with validation and calculation.

--- Conditionals Assignment/js/script.js
+++ Conditionals Assignment/js/script.js
@@ -5,3 +5,12 @@ Conditionals Assignment
  */
 
 //alert("Test!");
+
+var entry = prompt("Did you want to enter the venue today?");
+if(entry === "") {
+    entry = prompt("I'm sorry I didn't catch that. Did you want to enter the venue today?");
+    if (entry === "Yes"){
+        var childsAge = Number(prompt("Enter your CURRENT age here:"));
+        var timeRemaining = 18 - childsAge;
+
+        
