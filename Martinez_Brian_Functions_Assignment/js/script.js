b3b8d21f2cc6005b72bb7ac9b7fa5e1739e362c3
b-martinez <bamartinez@fullsail.edu>
2015-04-30 15:45:47 -0700

    Normal Function
    Normal function added.

--- Martinez_Brian_Functions_Assignment/js/script.js
+++ Martinez_Brian_Functions_Assignment/js/script.js
@@ -10,6 +10,20 @@
 var price = Number(prompt("Enter the price of item you are purchasing."));
 var quantity = Number(prompt("Enter the quantity of the item you are purchasing."));
 var salesTax = Number(prompt("Enter the sales tax of the item you are purchasing.\nPlease enter in decimal form."));
+var discount = Number(prompt("Enter any discount (%) if applicable.\n(Discount will be applied after taxes."));
+
+var purchase = totalCost(price, quantity, salesTax);
+function totalCost(price, quantity, salesTax){
+    var subTotal = (price * quantity);
+    var tax = (subTotal * salesTax);
+    var purchase = subTotal + tax;
+    return purchase;
+
+}
+
+totalCost(price, quantity, salesTax);
+
+console.log("The total price of your purchase is $" + purchase + ".");
 
 
 
