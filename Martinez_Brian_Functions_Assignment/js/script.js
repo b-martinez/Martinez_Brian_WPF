265b0f9bdf5d2b2ae0f5e314177005287805d9dd
b-martinez <bamartinez@fullsail.edu>
2015-04-30 16:17:30 -0700

    Test Values

--- Martinez_Brian_Functions_Assignment/js/script.js
+++ Martinez_Brian_Functions_Assignment/js/script.js
@@ -10,7 +10,7 @@
 var price = Number(prompt("Enter the price of item you are purchasing."));
 var quantity = Number(prompt("Enter the quantity of the item you are purchasing."));
 var salesTax = Number(prompt("Enter the sales tax of the item you are purchasing.\nPlease enter in decimal form."));
-var discount = Number(prompt("Enter any discount (%) if applicable.\n(Discount will be applied after taxes."));
+var discount = Number(prompt("Enter any discount (%) if applicable.\n(Discount will be applied after taxes.)"));
 
 var purchase = totalCost(price, quantity, salesTax);
 function totalCost(price, quantity, salesTax){
@@ -36,8 +36,11 @@ var newAmount = discountCost(purchase, discount);
 
 console.log("Your new amount after the discount is applied is $" + newAmount + ".");
 
-
-
+/*Test Values
+I typed in 2 for price of item, 2 for quantity, .10 for sales tax and 50 for discount. Calculator returned $4.4 for my total cost and $2.2 for my new cost after discount.
+ I typed in 29.99 for price of item, 3 for quantity, .0875 for sales tax and 0 for discount. Calculator returned $97.84 for my total cost and $97.84 for my new cost after discount.
+ I typed in 5 for price of item, 10 for quantity, .09 for sales tax and 25 for discount. Calculator returned $54.50 for my total cost and $40.88 for my new cost after discount.
+ */
 
 
 
